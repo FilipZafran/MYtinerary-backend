e65d49628ce2a331f3a9cea48d9d84a852b4231e
FichoZee <filip.zafran@gmail.com>
2020-02-24 11:51:24 +0100

    working, need to style

--- routes/cities.js
+++ routes/cities.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cityModel = require("../model/cityModel");
 
-// router.get("/test", (req, res) => {
-//   res.send({ msg: "Cities test route." });
-// });
-
 router.get("/all", (req, res) => {
   cityModel
     .find({})
